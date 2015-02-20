08c14365022549541a4b4c3dc67693cc0b3f88d3
Aurora Nou <auroranou@gmail.com>
2015-02-19 21:20:21 -0500

    add half of slides, map icons, icon map

---
+++ map_icons.js
@@ -0,0 +1,86 @@
+var mapIcons = [{
+	"type": "Feature", 
+	"geometry": {
+		"type": "Point",
+		"coordinates": [-90, 40]
+		},
+	"properties": {
+		"title": "Java",
+		"description": "Creator: James Gosling",
+		"icon": {
+			"iconUrl": "./icons/Java.png",
+			"iconSize": [50, 50],
+			"iconAnchor": [25, 25],
+			"popupAnchor": [0, -25],
+			"className": "icon"
+		}
+	}
+}, {
+	"type": "Feature", 
+	"geometry": {
+		"type": "Point",
+		"coordinates": [-110, 40]
+	},
+	"properties": {
+		"title": "JavaScript",
+		"description": "Creator: Brendan Eich",
+		"icon": {
+			"iconUrl": "./icons/JavaScript.png",
+			"iconSize": [50, 50],
+			"iconAnchor": [25, 25],
+			"popupAnchor": [0, -25],
+			"className": "icon"
+		}
+	}
+}, {
+	"type": "Feature", 
+	"geometry": {
+		"type": "Point",
+		"coordinates": [-40.465461,71.824364]
+	},
+	"properties": {
+		"title": "PHP",
+		"description": "Creator: Rasmus Lerdorf",
+		"icon": {
+			"iconUrl": "./icons/PHP.png",
+			"iconSize": [50, 50],
+			"iconAnchor": [25, 25],
+			"popupAnchor": [0, -25],
+			"className": "icon"
+		}
+	}
+}, {
+	"type": "Feature", 
+	"geometry": {
+		"type": "Point",
+		"coordinates": [5.590768,52.147946]
+	}, 
+	"properties": {
+		"title": "Python",
+		"description": "Creator: Guido van Rossum",
+		"icon": {
+			"iconUrl": "./icons/Python.png",
+			"iconSize": [50, 50],
+			"iconAnchor": [25, 25],
+			"popupAnchor": [0, -25],
+			"className": "icon"
+		}
+	}
+}, {
+	"type": "Feature", 
+	"geometry": {
+		"type": "Point",
+		"coordinates": [143.325245,43.469542]
+	},
+	"properties": {
+		"title": "Ruby",
+		"description": "Creator: Yukihiro Matsumoto",
+		"icon": {
+			"iconUrl": "./icons/Ruby.png",
+			"iconSize": [50, 50],
+			"iconAnchor": [25, 25],
+			"popupAnchor": [0, -25],
+			"className": "icon"
+		}
+	}
+}]
